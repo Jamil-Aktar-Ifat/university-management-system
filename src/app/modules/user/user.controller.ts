56d9f93a9eee39df32a787f0b849f57515442067
jamil-dev <i>
2024-12-13 14:00:09 +0000

    refactored user controller and service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,12 +1,15 @@
+import { UserServices } from './user.service';
+
 const createStudent = async (req: Request, res: Response) => {
   try {
+    const { password, student: studentData } = req.body;
 
-    const { student: studentData } = req.body;
+    // const zodParsedData = studentValidationSchema.parse(studentData);
 
-    const zodParsedData = studentValidationSchema.parse(studentData);
-
-
-    const result = await StudentServices.createStudentIntoDB(zodParsedData); 
+    const result = await UserServices.createStudentIntoDB(
+      password,
+      studentData,
+    );
 
     // send response
     res.status(200).json({
